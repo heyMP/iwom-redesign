db066a5fb26ce3f80d64d1e139ac774c34fff18c
Michael Potter <mgp140@psu.edu>
2014-11-26 21:35:38 -0500

    Added tabs and color palette

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,10 +1,17 @@
-var eqjs = require('../bower_components/eq.js/dist/eq.min.js');
-
 (function ($) {
   // Nav
   $(document).ready(function() {
-    $('.nav-trigger a').click(function() {
+    $('.nav-trigger a').on('click', function() {
       $('.nav__menu').toggleClass('opened');
+    });
+  });
+
+  // Accessible Tabs
+  $(document).ready(function(){
+    $(".tabs").accessibleTabs({
+        tabhead:'.tabs__head',
+        fx:"fadeIn",
+        tabbody: '.tabs__body'
     });
   });
 })(jQuery);
