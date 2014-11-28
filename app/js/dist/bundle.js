6d1fe2b418aa013ea8bb3af6e7896cdbfd11b6d0
Michael Potter <mgp140@psu.edu>
2014-11-28 13:53:20 -0500

    Added the Gallery and Lightbox components

--- app/js/dist/bundle.js
+++ app/js/dist/bundle.js
@@ -1,20 +1,53 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function ($) {
   // Nav
-  $(document).ready(function() {
+  function navToggle() {
     $('.nav-trigger a').on('click', function() {
       $('.nav__menu').toggleClass('opened');
     });
+  }
+  $(document).ready(function() {
+    navToggle();
   });
 
   // Accessible Tabs
-  $(document).ready(function(){
+  function accessibleTabs() {
     $(".tabs").accessibleTabs({
         tabhead:'.tabs__head',
         fx:"fadeIn",
         tabbody: '.tabs__body'
     });
+  }
+  $(document).ready(function(){
+    accessibleTabs();
   });
+
+  // Swiper Gallery
+  function swiperGallery() {
+    var $swiper = $('.swiper-container');
+    var pager = $swiper.hasClass('swiper--pager') === true ? true : false;
+
+    $swiper.swiper({
+      speed : 500,
+      calculateHeight: true,
+      keyboardControl: true,
+      pagination: '.swiper-pager',
+      paginationClickable: pager,
+    });
+  }
+  $(document).ready(function(){
+    swiperGallery();
+  })
+
+  // Swipebox
+  function swipeboxLightbox() {
+    $('.swipebox').swipebox();
+  }
+  $(document).ready(function(){
+    swipeboxLightbox({
+      hideCloseButtonOnMobile : false,
+    });
+  })
 })(jQuery);
 
 },{}]},{},[1]);
--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,17 +1,50 @@
 (function ($) {
   // Nav
-  $(document).ready(function() {
+  function navToggle() {
     $('.nav-trigger a').on('click', function() {
       $('.nav__menu').toggleClass('opened');
     });
+  }
+  $(document).ready(function() {
+    navToggle();
   });
 
   // Accessible Tabs
-  $(document).ready(function(){
+  function accessibleTabs() {
     $(".tabs").accessibleTabs({
         tabhead:'.tabs__head',
         fx:"fadeIn",
         tabbody: '.tabs__body'
     });
+  }
+  $(document).ready(function(){
+    accessibleTabs();
   });
+
+  // Swiper Gallery
+  function swiperGallery() {
+    var $swiper = $('.swiper-container');
+    var pager = $swiper.hasClass('swiper--pager') === true ? true : false;
+
+    $swiper.swiper({
+      speed : 500,
+      calculateHeight: true,
+      keyboardControl: true,
+      pagination: '.swiper-pager',
+      paginationClickable: pager,
+    });
+  }
+  $(document).ready(function(){
+    swiperGallery();
+  })
+
+  // Swipebox
+  function swipeboxLightbox() {
+    $('.swipebox').swipebox();
+  }
+  $(document).ready(function(){
+    swipeboxLightbox({
+      hideCloseButtonOnMobile : false,
+    });
+  })
 })(jQuery);
